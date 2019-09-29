29c26d9e7a7edeebc2e0296c76b1cbcae9634fbb
Neel Venugopal <neelv7@berkeley.edu>
2019-09-29 01:19:49 -0500

    Updates

---
+++ precache-manifest.0b8d0cbebd77bece3358e9a478871857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06220d371cdb45cf8523ef4fe506d012",
+    "url": "/medhacks/index.html"
+  },
+  {
+    "revision": "1d835f3136fc63147fe8",
+    "url": "/medhacks/static/css/main.815e94b6.chunk.css"
+  },
+  {
+    "revision": "d571c1841a0ba89419c2",
+    "url": "/medhacks/static/js/2.cf8996ee.chunk.js"
+  },
+  {
+    "revision": "1d835f3136fc63147fe8",
+    "url": "/medhacks/static/js/main.b7d13fe9.chunk.js"
+  },
+  {
+    "revision": "da5bea444458a9089812",
+    "url": "/medhacks/static/js/runtime-main.41701304.js"
+  }
+]);
